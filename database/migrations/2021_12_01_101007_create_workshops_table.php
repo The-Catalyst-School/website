9d52336e981ea8d2eae06b774bc6a6ba6ff44dbd
Lucio Baglione <coire1@gmail.com>
2021-12-01 11:57:22 +0100

    Scaffold basic models.

---
+++ database/migrations/2021_12_01_101007_create_workshops_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkshopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workshops', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('slug');
+            $table->string('title')->nullable();
+            $table->string('subtitle')->nullable();
+            $table->text('content')->nullable();
+            $table->date('scheduled_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workshops');
+    }
+}
