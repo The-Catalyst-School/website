dbd063d868c445d2b058bdb99d73e647b2d0332c
Lucio Baglione <coire1@gmail.com>
2022-02-20 20:04:43 +0100

    Add version to mix.

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,11 @@ const mix = require('laravel-mix');
 mix
   .js('resources/js/app.js', 'public/js').vue()
   .sass('resources/sass/app.scss', 'public/css')
-  .browserSync('http://localhost:8000/');
+
+if (!mix.inProduction()) {
+  mix.browserSync('http://localhost:8000/');
+}
+
+if (mix.inProduction()) {
+  mix.version();
+}
